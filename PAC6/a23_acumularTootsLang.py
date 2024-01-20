ec167aeb3c67e50de01a63f7f4eb395d017d4bba
Xavier Maltas Tarridas WS <xavi.maltass@gmail.com>
2024-01-20 17:57:24 +0100

    PAC6 - a2_3 done

--- PAC6/a23_acumularTootsLang.py
+++ PAC6/a23_acumularTootsLang.py
@@ -1,3 +1,6 @@
+# a23_acumularTootsLang.py
+# python3 a23_acumularTootsLang.py
+
 import findspark
 findspark.init()
 
@@ -7,7 +10,7 @@ from pyspark.streaming.kafka import KafkaUtils
 import json
 
 # Initialize SparkContext and StreamingContext with a 1-second batch interval
-app_name = <FILLIN>  # Name of your application
+app_name = "AccumulateTootsLang"  # Name of your application
 
 # Create the SparkContext
 try:
@@ -19,36 +22,35 @@ except ValueError:
 sc.setLogLevel("ERROR")
 
 batch_interval = 5  # Batch interval in seconds
-ssc = StreamingContext(<FILLIN>)
+ssc = StreamingContext(sc, batch_interval)
 ssc.checkpoint("checkpoint")  # Necessary for updateStateByKey operation
 
 # Define Kafka parameters
-kafka_server = <FILLIN>  # Kafka server address
-kafka_topic = <FILLIN>   # Kafka topic
-kafka_group = <FILLIN>   # Kafka consumer group, first surname of each member of the group separated by an underscore.
+kafka_server = 'Cloudera02:9092,Cloudera03:9092'  # Kafka server address
+kafka_topic = 'mastodon_toots'   # Kafka topic
+kafka_group = 'xmaltast'   # Kafka consumer group, first surname of each member of the group separated by an underscore.
 
 kafkaParams = {
     "metadata.broker.list": kafka_server,
     "group.id": kafka_group
 } 
 
-
 # Create a DStream that connects to Kafka
-kafkaStream = KafkaUtils.createDirectStream(<FILLIN>)
+kafkaStream = KafkaUtils.createDirectStream(ssc, [kafka_topic], kafkaParams)
 
 # Update the cumulative count using updateStateByKey
 def updateFunction(newValues, runningCount):
-    <FILLIN>
-    return <FILLIN>
+    if runningCount is None:
+        runningCount = 0
+    return sum(newValues) + runningCount
 
 # Count each toot as 1 and update the total count
 tootCounts = kafkaStream\
     .map(lambda x: json.loads(x[1]))\
-    .<FILLIN>
-    ....
-    .<FILLIN>
-    .updateStateByKey(<FILLIN>)\
-    <FILLIN>
+    .filter(lambda toot: "language" in toot and toot["language"] is not None)\
+    .map(lambda toot: (toot["language"], 1))\
+    .updateStateByKey(updateFunction)\
+    .transform(lambda rdd: rdd.sortBy(lambda x: x[1], ascending=False))
 
 # Print the cumulative count
 tootCounts.pprint()
