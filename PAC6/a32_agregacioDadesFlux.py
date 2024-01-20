04ebbe68e8e3aefae3f5a7c2036ed2b9bacb6221
Xavier Maltas Tarridas WS <xavi.maltass@gmail.com>
2024-01-20 21:07:04 +0100

    PAC6 - a3_2 start

---
+++ PAC6/a32_agregacioDadesFlux.py
@@ -0,0 +1,79 @@
+# a32_agregacioDadesFlux.py
+# python3 a32_agregacioDadesFlux.py
+
+import findspark
+findspark.init()
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StringType, StructType, StructField
+from pyspark import SparkConf, SparkContext
+from pyspark.sql.functions import from_json, col, window
+from pyspark.sql import SparkSession
+
+conf = SparkConf()
+conf.setMaster("local[2]")
+sc = SparkContext(conf=conf)
+sc.setLogLevel("ERROR")
+
+# Initialize Spark Session for Structured Streaming
+app_name = "activity3_2" + <FILLIN> # Replace with your Spark app name must include the username of the members of the group
+
+spark = SparkSession \
+    .builder \
+    .appName(app_name) \
+    .getOrCreate()
+
+# Define Kafka parameters
+kafka_topic = <FILLIN>
+kafka_bootstrap_servers = <FILLIN>  # Replace with your Kafka bootstrap servers
+
+# Read a small batch of data from Kafka for schema inference!
+batch_df = spark \
+    .read \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+    .option("subscribe", kafka_topic) \
+    .option("startingOffsets", "earliest") \
+    .option("endingOffsets", "{\"" + kafka_topic + "\":{\"0\":10}}") \
+    .load()
+
+# Infer schema
+schema = spark.read.json(batch_df.selectExpr("CAST(value AS STRING)").rdd.map(lambda x: x[0])).schema
+
+# Create streaming DataFrame by reading data from Kafka
+toots = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+    .option("subscribe", kafka_topic) \
+    .option("startingOffsets", <FILLIN>) \
+    .load()
+
+# Parse the value column as JSON and apply the infered schema. Then select the columns we need.
+toots_df = toots\
+    .select(from_json(col("value").cast("string"), schema).alias("parsed_value"))\
+    .filter(<FILLIN>)\
+    .select(<FILLIN>)\
+    .groupBy(<FILLIN>)\
+    .<FILLIN>
+    ...
+    <FILLIN>
+    
+
+try:
+    # Open stream to console (you need to execute it in a terminal to see the output)
+    query = toots_df \
+            .writeStream \
+            .outputMode(<FILLIN>) \
+            .format("console")\
+            .<FILLIN>
+            ...
+            .<FILLIN>
+            .trigger(<FILLIN>)\
+            .start()\
+
+    query.awaitTermination()
+except KeyboardInterrupt:
+    query.stop()
+    spark.stop()
+    sc.stop()
