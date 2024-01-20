b66cfdb8ae76118f8b82dc2d04f88a9befda89cb
Xavier Maltas Tarridas WS <xavi.maltass@gmail.com>
2024-01-20 21:39:19 +0100

    PAC6 - a3_2 done

--- PAC6/a32_agregacioDadesFlux.py
+++ PAC6/a32_agregacioDadesFlux.py
@@ -7,8 +7,7 @@ findspark.init()
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StringType, StructType, StructField
 from pyspark import SparkConf, SparkContext
-from pyspark.sql.functions import from_json, col, window
-from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col, window, count
 
 conf = SparkConf()
 conf.setMaster("local[2]")
@@ -16,7 +15,7 @@ sc = SparkContext(conf=conf)
 sc.setLogLevel("ERROR")
 
 # Initialize Spark Session for Structured Streaming
-app_name = "activity3_2" + <FILLIN> # Replace with your Spark app name must include the username of the members of the group
+app_name = "activity3_2_xmaltast"  # Replace with your Spark app name, must include the username of the members of the group
 
 spark = SparkSession \
     .builder \
@@ -24,8 +23,8 @@ spark = SparkSession \
     .getOrCreate()
 
 # Define Kafka parameters
-kafka_topic = <FILLIN>
-kafka_bootstrap_servers = <FILLIN>  # Replace with your Kafka bootstrap servers
+kafka_topic = 'mastodon_toots'
+kafka_bootstrap_servers = 'Cloudera02:9092,Cloudera03:9092'  # Replace with your Kafka bootstrap servers
 
 # Read a small batch of data from Kafka for schema inference!
 batch_df = spark \
@@ -34,7 +33,7 @@ batch_df = spark \
     .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
     .option("subscribe", kafka_topic) \
     .option("startingOffsets", "earliest") \
-    .option("endingOffsets", "{\"" + kafka_topic + "\":{\"0\":10}}") \
+    .option("endingOffsets", '{"' + kafka_topic + '":{"0": -1}}') \
     .load()
 
 # Infer schema
@@ -46,31 +45,27 @@ toots = spark \
     .format("kafka") \
     .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
     .option("subscribe", kafka_topic) \
-    .option("startingOffsets", <FILLIN>) \
+    .option("startingOffsets", "earliest") \
     .load()
 
-# Parse the value column as JSON and apply the infered schema. Then select the columns we need.
-toots_df = toots\
-    .select(from_json(col("value").cast("string"), schema).alias("parsed_value"))\
-    .filter(<FILLIN>)\
-    .select(<FILLIN>)\
-    .groupBy(<FILLIN>)\
-    .<FILLIN>
-    ...
-    <FILLIN>
-    
+# Parse the value column as JSON and apply the inferred schema. Then select the columns we need.
+toots_df = toots \
+    .select(from_json(col("value").cast("string"), schema).alias("parsed_value")) \
+    .filter("parsed_value is not null and parsed_value.language is not null") \
+    .select("parsed_value.language") \
+    .groupBy("language") \
+    .agg(count("*").alias("count")) \
+    .orderBy(col("count").desc())  # Order by the count in descending order
 
 try:
     # Open stream to console (you need to execute it in a terminal to see the output)
     query = toots_df \
-            .writeStream \
-            .outputMode(<FILLIN>) \
-            .format("console")\
-            .<FILLIN>
-            ...
-            .<FILLIN>
-            .trigger(<FILLIN>)\
-            .start()\
+        .writeStream \
+        .outputMode("complete") \
+        .format("console") \
+        .option("truncate", "false") \
+        .trigger(processingTime="10 seconds") \
+        .start()
 
     query.awaitTermination()
 except KeyboardInterrupt:
