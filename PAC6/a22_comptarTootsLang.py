a0d1821cff336fb05c6682182606d69812b6be45
Xavier Maltas Tarridas WS <xavi.maltass@gmail.com>
2024-01-20 17:36:41 +0100

    PAC6 - a2_2 done

--- PAC6/a22_comptarTootsLang.py
+++ PAC6/a22_comptarTootsLang.py
@@ -1,3 +1,6 @@
+# a22_comptarTootsLang.py
+# python3 a22_comptarTootsLang.py
+
 import findspark
 findspark.init()
 
@@ -7,7 +10,7 @@ from pyspark.streaming.kafka import KafkaUtils
 import json
 
 # Initialize SparkContext and StreamingContext with a 1-second batch interval
-app_name = "FILLIN"  # Name of your application
+app_name = "TopLanguagesToots"  # Name of your application
 
 # Create the SparkContext
 try:
@@ -19,13 +22,13 @@ sc.setLogLevel("ERROR")
 
 # Create the StreamingContext
 batch_interval = 5  # Batch interval in seconds
-ssc = StreamingContext(<FILLIN>)
+ssc = StreamingContext(sc, batch_interval)
 ssc.checkpoint("checkpoint")  # Necessary for updateStateByKey operation
 
 # Define Kafka parameters
-kafka_server = <FILLIN>  # Kafka server address
-kafka_topic = <FILLIN>   # Kafka topic
-kafka_group = <FILLIN>   # Kafka consumer group, first surname of each member of the group separated by an underscore.
+kafka_server = 'Cloudera02:9092,Cloudera03:9092'  # Kafka server address
+kafka_topic = 'mastodon_toots'   # Kafka topic
+kafka_group = 'xmaltast'   # Kafka consumer group, first surname of each member of the group separated by an underscore.
 
 kafkaParams = {
     "metadata.broker.list": kafka_server,
@@ -33,14 +36,17 @@ kafkaParams = {
 } 
 
 # Create a DStream that connects to Kafka
-kafkaStream = KafkaUtils.createDirectStream(<FILLIN>)
+kafkaStream = KafkaUtils.createDirectStream(ssc, [kafka_topic], kafkaParams)
 
 # Count the number of toots per language
-tootLangCounts = kafkaStream\
-    .<FILLIN>
+tootLangCounts = kafkaStream \
+    .map(lambda toot: json.loads(toot[1])) \
+    .map(lambda toot: (toot.get("language", "Unknown"), 1)) \
+    .updateStateByKey(lambda new_counts, current_count: sum(new_counts) + (current_count or 0)) \
+    .transform(lambda rdd: rdd.sortBy(lambda x: x[1], ascending=False))
 
 # Print the cumulative count
-tootLangCounts.pprint(<FILLIN>)
+tootLangCounts.pprint()
 
 # Start the computation
 try:
